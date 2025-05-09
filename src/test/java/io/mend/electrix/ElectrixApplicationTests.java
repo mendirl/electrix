b479f79f8592b171dd0c4aa852585da782516bb5
Fabien Couillard <couillard.fabien+dev@gmail.com>
2025-05-09 15:51:05 +0200

    changes

--- src/test/java/io/mend/electrix/ElectrixApplicationTests.java
+++ src/test/java/io/mend/electrix/ElectrixApplicationTests.java
@@ -1,9 +1,11 @@
 package io.mend.electrix;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+@Disabled
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ElectrixApplicationTests {
