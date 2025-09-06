75f17166ea76062b81815d89a20c840d375a01c6
Fabien Couillard <couillard.fabien+dev@gmail.com>
2025-09-06 17:32:40 +0200

    update

--- src/main/java/io/mend/electrix/config/WebSecurityConfiguration.java
+++ src/main/java/io/mend/electrix/config/WebSecurityConfiguration.java
@@ -1,6 +1,6 @@
 package io.mend.electrix.config;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.security.autoconfigure.actuate.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
