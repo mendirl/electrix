01169ca694a429c0afe365c7614399c664bc2b12
Fabien Couillard <couillard.fabien+dev@gmail.com>
2026-04-04 18:03:48 +0200

    merge

---
+++ mcp/mcp-server/src/main/java/io/mendirl/mcpserver/McpServerApplication.java
@@ -0,0 +1,11 @@
+package io.mendirl.mcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerApplication {
+    static void main() {
+        SpringApplication.run(McpServerApplication.class);
+    }
+}
